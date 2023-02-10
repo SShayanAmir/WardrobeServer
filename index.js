0c75c05dd96f63de2befd56ec41fb55732385f6a
SShayanAmir <sshayan.aamir@gmail.com>
2023-02-11 02:40:06 +0500

    first commit

--- index.js
+++ index.js
@@ -211,4 +211,8 @@ app.put("/customer/order/:id", checkJwt, async (req, res) => {
     }
 })
 
-app.listen(process.env.SERVER_PORT, () => console.log("server listening on port " + process.env.SERVER_PORT))
+app.get("/food", async(req, res) => {
+    res.send("Its working!!!")
+})
+    
+app.listen(process.env.PORT, () => console.log("server listening on port " + process.env.PORT))
